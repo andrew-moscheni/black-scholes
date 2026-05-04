bd95c28eaf4fad482c0a776d03c7ccbed322d3ac
andrew-moscheni <agmoscheni@gmail.com>
2026-05-03 20:17:10 -0400

    Initial commit: Black-Scholes project with CMake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/agmos/projects/black-scholes
+# Build directory: C:/Users/agmos/projects/black-scholes/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
